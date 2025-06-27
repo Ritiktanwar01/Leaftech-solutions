3b63492eb42aa89e3eebe4f52fffbedf728bfe33
Ritik_tanwar_01 <tanwarritik999@gmail.com>
2025-06-27 13:14:44 +0530

    not found added

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+  return (
+    <main style={{ textAlign: 'center', padding: '5rem' }}>
+      <h1>🚧 Under Maintenance</h1>
+      <p>This page is temporarily unavailable. Please check back later.</p>
+    </main>
+  );
+}
